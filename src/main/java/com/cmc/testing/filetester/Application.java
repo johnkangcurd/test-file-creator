17df7f66b7f38f29fc4262daa33811fbb112470e
johnkangcurd <johnkangcurd@gmail.com>
2016-02-02 13:25:51 -0500

    Update Application.java

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        int i = 1/0;
         SpringApplication.run(Application.class, args);
     }
 }
