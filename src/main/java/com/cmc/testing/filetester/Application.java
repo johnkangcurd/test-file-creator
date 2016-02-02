792e536f4a578278f4db644c73cf2b69670f86d6
johnkangcurd <johnkangcurd@gmail.com>
2016-02-02 16:08:59 -0500

    clean slate

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        //fix all the codes
         SpringApplication.run(Application.class, args);
     }
 }
