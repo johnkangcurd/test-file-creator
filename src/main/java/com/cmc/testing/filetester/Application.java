8645dd086b9f76a15ad40e7387ecaaac09051208
johnkangcurd <johnkangcurd@gmail.com>
2016-02-03 14:27:07 -0500

    too messy

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    //wednesday starting point
-    //change for jason
-    //another commit
-    //lets make it three
+    //too messy
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
