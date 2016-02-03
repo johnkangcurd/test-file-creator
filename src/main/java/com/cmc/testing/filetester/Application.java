3208d11d32105c46bc6dd1b02e125843c61d4409
johnkangcurd <johnkangcurd@gmail.com>
2016-02-03 14:20:17 -0500

    change for jason

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     //wednesday starting point
-    //change for build
+    //change for jason
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
