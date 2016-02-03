53f683dbc8f1e6f35893b45b0e977cb8a2ae8d7e
johnkangcurd <johnkangcurd@gmail.com>
2016-02-03 14:30:41 -0500

    breaking code.

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     //too messy
+    breaking this code
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
