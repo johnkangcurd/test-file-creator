92a68e3c2c21ab2c819faf2204534cbc2a88898a
johnkangcurd <johnkangcurd@gmail.com>
2016-02-04 12:59:10 -0500

    test thursday

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    //showcase for jason
+//test
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
