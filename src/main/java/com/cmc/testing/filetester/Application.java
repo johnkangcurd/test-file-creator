471c60ea4f1f2f748ff7c0cca03fe67a2519527c
johnkangcurd <johnkangcurd@gmail.com>
2016-02-05 10:35:46 -0500

    Update Application.java

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    //fixing sync
+    //test sync
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
