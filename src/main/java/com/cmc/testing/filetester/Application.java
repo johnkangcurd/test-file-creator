63cc3f978ebc15158209f2e7e1b60d742e696ec7
johnkangcurd <johnkangcurd@gmail.com>
2016-02-05 11:44:25 -0500

    final fix

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-final break
+//final fix
 public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
