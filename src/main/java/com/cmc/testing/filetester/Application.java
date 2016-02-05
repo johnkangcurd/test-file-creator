6ec1d5a1355a24f57130ed576ff418955ea05d95
johnkangcurd <johnkangcurd@gmail.com>
2016-02-05 11:43:15 -0500

    final break test

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-//another break
+final break
 public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
-//third commit  
