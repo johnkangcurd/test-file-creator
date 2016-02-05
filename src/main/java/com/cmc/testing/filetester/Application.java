987d369c278c883768bd61064f1c975d1a8a386a
johnkangcurd <johnkangcurd@gmail.com>
2016-02-05 11:20:43 -0500

    breaking it good

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    breaking it good
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
