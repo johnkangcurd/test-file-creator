b204fea3ed45a296dbaedc663c24f121f496b334
johnkangcurd <johnkangcurd@gmail.com>
2016-02-05 11:02:44 -0500

    CLEAN SLATE

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-//fix sync 2
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
